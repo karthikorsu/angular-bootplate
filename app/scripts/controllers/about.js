0362e1a4b39cb76e1c969a05f95da27b6138f9e1
Orsu Karthik <kartik.orsu0503@gmail.com>
2017-06-07 21:20:53 +0530

    ANGULAR-BOOTPLATE basic skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootplateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBootplateApp
+ */
+angular.module('angularBootplateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBootplateApp
+ */
+angular.module('angularBootplateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
